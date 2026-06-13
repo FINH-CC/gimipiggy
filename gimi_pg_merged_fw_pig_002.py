99f626154f4b8edd4938c604366b91327d5fd5f0
dostr63 <63859553+dostr63@users.noreply.github.com>
2026-06-13 21:10:07 +0100

    Flash binaries for pig_001, pig_002, pig_003, and README instructions on how to re-flash.

--- gimi_pg_merged_fw_pig_002.py
+++ gimi_pg_merged_fw_pig_002.py
@@ -7,7 +7,7 @@ esptool --chip esp32 merge-bin \
   --flash-mode dio \
   --flash-freq 80m \
   --flash-size 4MB \
-  0x1000  ~/Documents/PlatformIO/Projects/finh_gimi/.pio/build/esp32dev/bootloader.bin \
-  0x8000  ~/Documents/PlatformIO/Projects/finh_gimi/.pio/build/esp32dev/partitions.bin \
+  0x1000  ~/Documents/PlatformIO/Projects/gimipiggy/gimipiggy/.pio/build/esp32dev/bootloader.bin \
+  0x8000  ~/Documents/PlatformIO/Projects/gimipiggy/gimipiggy/.pio/build/esp32dev/partitions.bin \
   0xe000  ~/.platformio/packages/framework-arduinoespressif32/tools/partitions/boot_app0.bin \
-  0x10000 ~/Documents/PlatformIO/Projects/finh_gimi/.pio/build/esp32dev/firmware.bin
+  0x10000 ~/Documents/PlatformIO/Projects/gimipiggy/gimipiggy/.pio/build/esp32dev/firmware.bin
