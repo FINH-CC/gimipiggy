ea25dbb41d294b7e4261e4be5a5e8f38702193db
dostr63 <63859553+dostr63@users.noreply.github.com>
2026-06-13 19:56:51 +0100

    Added Python scripts to build single .bin files for each of pig_001, pig_002, Pig_003 along with preprocessor #define for each of these.

---
+++ gimi_pg_merged_fw_pig_001.py
@@ -0,0 +1,13 @@
+#
+# Builds a single flash file for ESP32.
+# Locate the output file at 0x0000
+#
+esptool --chip esp32 merge-bin \
+  -o merged_firmware_pig_001.bin \
+  --flash-mode dio \
+  --flash-freq 80m \
+  --flash-size 4MB \
+  0x1000  ~/Documents/PlatformIO/Projects/finh_gimi/.pio/build/esp32dev/bootloader.bin \
+  0x8000  ~/Documents/PlatformIO/Projects/finh_gimi/.pio/build/esp32dev/partitions.bin \
+  0xe000  ~/.platformio/packages/framework-arduinoespressif32/tools/partitions/boot_app0.bin \
+  0x10000 ~/Documents/PlatformIO/Projects/finh_gimi/.pio/build/esp32dev/firmware.bin
--- include/gimi_pb_server.h
+++ include/gimi_pb_server.h
@@ -5,6 +5,12 @@
 #ifndef __GIMI_PB_SERVER_H
 #define __GIMI_PB_SERVER_H
 
+// Uncomment only ONE of the following #define options:
+#define BUILD_TARGET_PIG_001
+//#define BUILD_TARGET_PIG_002
+//#define BUILD_TARGET_PIG_003
+//#define BUILD_TARGET_GITHUB_TEST
+
 #define SLEEP_TIME_BETWEEN_FETCH 25000000 // In microseconds. 1,000,000 us= 1 second. 25000000 = 25 seconds.
 
 // Receipt are downloaded immediately before printing, to ensure an up-to-date-copy.
@@ -29,6 +35,8 @@ enum gimi_pb_receipt_type {
 
 // Uncomment ONLY ONE block of URL's below, to select which server (and/or which user profile that you want THIS device to use).
 
+#if defined (BUILD_TARGET_PIG_001)
+
 // pig_001 Real live FINH server.
 #define GIMI_PB_RECEIPT_WELCOME_URL      "https://pudixovgv7mqdvud.public.blob.vercel-storage.com/pig_001/welcome.bin"
 #define GIMI_PB_RECEIPT_DEFAULT_URL      "https://pudixovgv7mqdvud.public.blob.vercel-storage.com/pig_001/default.bin"
@@ -37,29 +45,42 @@ enum gimi_pb_receipt_type {
 #define GIMI_PB_RECEIPT_REMINDER_URL     "https://pudixovgv7mqdvud.public.blob.vercel-storage.com/pig_001/reminder.bin"
 #define GIMI_PB_RECEIPT_NOTIFICATION_URL "https://pudixovgv7mqdvud.public.blob.vercel-storage.com/pig_001/notification.bin"
 
+#elif defined (BUILD_TARGET_PIG_002)
+
 // pig_002 Real live FINH server.
-/*#define GIMI_PB_RECEIPT_WELCOME_URL      "https://pudixovgv7mqdvud.public.blob.vercel-storage.com/pig_002/welcome.bin"
-#define GIMI_PB_RECEIPT_DEAFULT_URL      "https://pudixovgv7mqdvud.public.blob.vercel-storage.com/pig_002/default.bin"
+#define GIMI_PB_RECEIPT_WELCOME_URL      "https://pudixovgv7mqdvud.public.blob.vercel-storage.com/pig_002/welcome.bin"
+#define GIMI_PB_RECEIPT_DEFAULT_URL      "https://pudixovgv7mqdvud.public.blob.vercel-storage.com/pig_002/default.bin"
 #define GIMI_PB_RECEIPT_SAVINGS_URL      "https://pudixovgv7mqdvud.public.blob.vercel-storage.com/pig_002/savings.bin"
 #define GIMI_PB_RECEIPT_CELEBRATION_URL  "https://pudixovgv7mqdvud.public.blob.vercel-storage.com/pig_002/celebration.bin"
 #define GIMI_PB_RECEIPT_REMINDER_URL     "https://pudixovgv7mqdvud.public.blob.vercel-storage.com/pig_002/reminder.bin"
-#define GIMI_PB_RECEIPT_NOTIFICATION_URL "https://pudixovgv7mqdvud.public.blob.vercel-storage.com/pig_002/notification.bin"*/
+#define GIMI_PB_RECEIPT_NOTIFICATION_URL "https://pudixovgv7mqdvud.public.blob.vercel-storage.com/pig_002/notification.bin"
+
+#elif defined (BUILD_TARGET_PIG_003)
 
 // pig_003 Real live FINH server.
-/*#define GIMI_PB_RECEIPT_WELCOME_URL     "https://pudixovgv7mqdvud.public.blob.vercel-storage.com/pig_003/welcome.bin"
+#define GIMI_PB_RECEIPT_WELCOME_URL     "https://pudixovgv7mqdvud.public.blob.vercel-storage.com/pig_003/welcome.bin"
 #define GIMI_PB_RECEIPT_DEFAULT_URL      "https://pudixovgv7mqdvud.public.blob.vercel-storage.com/pig_003/default.bin"
 #define GIMI_PB_RECEIPT_SAVINGS_URL      "https://pudixovgv7mqdvud.public.blob.vercel-storage.com/pig_003/savings.bin"
 #define GIMI_PB_RECEIPT_CELEBRATION_URL  "https://pudixovgv7mqdvud.public.blob.vercel-storage.com/pig_003/celebration.bin"
 #define GIMI_PB_RECEIPT_REMINDER_URL     "https://pudixovgv7mqdvud.public.blob.vercel-storage.com/pig_003/reminder.bin"
-#define GIMI_PB_RECEIPT_NOTIFICATION_URL "https://pudixovgv7mqdvud.public.blob.vercel-storage.com/pig_003/notification.bin"*/
+#define GIMI_PB_RECEIPT_NOTIFICATION_URL "https://pudixovgv7mqdvud.public.blob.vercel-storage.com/pig_003/notification.bin"
+
+#elif defined (BUILD_TARGET_GITHUB_TEST)
 
 // Dom's Github testbed.
-/*#define GIMI_PB_RECEIPT_WELCOME_URL      "https://raw.githubusercontent.com/dostr/dostr.github.io/master/w384_type_test_384_1047.bin"
+#define GIMI_PB_RECEIPT_WELCOME_URL      "https://raw.githubusercontent.com/dostr/dostr.github.io/master/w384_type_test_384_1047.bin"
 #define GIMI_PB_RECEIPT_DEFAULT_URL      "https://raw.githubusercontent.com/dostr/dostr.github.io/master/w384_type_test_384_1047.bin"
 #define GIMI_PB_RECEIPT_SAVINGS_URL      "https://raw.githubusercontent.com/dostr/dostr.github.io/master/w384_type_test_384_1047.bin"
 #define GIMI_PB_RECEIPT_CELEBRATION_URL  "https://raw.githubusercontent.com/dostr/dostr.github.io/master/w384_type_test_384_1047.bin"
 #define GIMI_PB_RECEIPT_REMINDER_URL     "https://raw.githubusercontent.com/dostr/dostr.github.io/master/w384_type_test_384_1047.bin"
-#define GIMI_PB_RECEIPT_NOTIFICATION_URL "https://raw.githubusercontent.com/dostr/dostr.github.io/master/w384_type_test_384_1047.bin"*/
+#define GIMI_PB_RECEIPT_NOTIFICATION_URL "https://raw.githubusercontent.com/dostr/dostr.github.io/master/w384_type_test_384_1047.bin"
+
+#else 
+
+# warning "BUILD_TARGET_PIG_XXX not defined in gimi_pb_server.h"
+
+#endif // BUILD_TARGET_PIG_XXX
+
 
 #endif // __GIMI_PB_SERVER_H
 
